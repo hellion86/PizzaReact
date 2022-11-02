c7a3a003ad8bf2586fef65528549834100f93e8f
Vyacheslav Mezhurevskiy <VMezhurevsky@gmail.com>
2022-11-02 21:15:11 +0700

    add fetch, add mockapi

--- src/App.js
+++ src/App.js
@@ -7,6 +7,19 @@ import pizzas from './assets/pizzas.json';
 import './scss/app.scss';
 
 function App() {
+  // https://6362739d66f75177ea2fed20.mockapi.io/items
+  const mockapiUrl = 'https://6362739d66f75177ea2fed20.mockapi.io/items';
+  const [items, setItems] = React.useState([]);
+  React.useEffect(() => {
+    fetch(mockapiUrl)
+      .then((data) => {
+        // console.log(data);
+        return data.json();
+      })
+      .then((data) => {
+        setItems(data);
+      });
+  }, []);
   return (
     <div className="wrapper">
       <Header />
@@ -18,7 +31,7 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((obj) => (
+            {items.map((obj) => (
               <PizzaCard key={obj.id} {...obj} />
             ))}
           </div>
